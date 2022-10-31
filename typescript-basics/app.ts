4df04686db1b1572e176c33f0983cb41d8119953
galbeiroc <alcresgu_1989@hotmail.com>
2022-10-30 21:06:38 -0500

    nested object

--- typescript-basics/app.ts
+++ typescript-basics/app.ts
@@ -4,3 +4,23 @@ const person = {
 }
 
 console.log(person.name)
+
+type TProduct = {
+  id: string;
+  price: number;
+  tags: string[];
+  details: {
+    title: string;
+    description: string;
+  }
+}
+
+const product: TProduct = {
+  id: 'abc1',
+  price: 12.99,
+  tags: ['great-offer', 'hot-and-new'],
+  details: {
+    title: 'Red carpet',
+    description: 'A great carpet - almost brand-new'
+  }
+}

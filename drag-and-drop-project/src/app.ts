b126bdea6746e9787066e8cf06cd7738f2fb4d40
galbeiroc <alcresgu_1989@hotmail.com>
2022-12-07 23:02:18 -0500

    render project list

--- drag-and-drop-project/src/app.ts
+++ drag-and-drop-project/src/app.ts
@@ -45,6 +45,35 @@ function autobind(
   return adjustedDescriptor;
 }
 
+// ProjectList Class
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: 'active' | 'finished') {
+    this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importNode = document.importNode(this.templateElement.content, true);
+    this.element = importNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-project-list`;
+    this.element.querySelector('ul')!.id = listId;
+    this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('beforeend', this.element);
+  }
+}
+
 // ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -118,3 +147,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePRJList = new ProjectList('active');
+const finishedPRJList = new ProjectList('finished');

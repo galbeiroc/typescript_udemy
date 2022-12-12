c009a9e61353e17394fbb08b996b219a82600c42
galbeiroc <alcresgu_1989@hotmail.com>
2022-12-11 19:05:50 -0500

    add classes and custom types

--- drag-and-drop-project/src/app.ts
+++ drag-and-drop-project/src/app.ts
@@ -1,7 +1,27 @@
+// Project Type
+enum ProjectStatus {
+  Active,
+  Finished
+}
+
+class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people: number,
+    public status: ProjectStatus
+  ) {
+
+  }
+}
+
 // Project State Management
+type Listener = (items: Project[]) => void;
+
 class ProjectState {
-  private listeners: any[] = [];
-  private projects: any[] = [];
+  private listeners: Listener[] = [];
+  private projects: Project[] = [];
   private static instance: ProjectState;
 
   private constructor() {}
@@ -14,17 +34,18 @@ class ProjectState {
     return this.instance;
   }
 
-  addListener(listenerFunc: Function) {
+  addListener(listenerFunc: Listener) {
     this.listeners.push(listenerFunc);
   }
 
   addProject(title: string, description: string, numPeople: number) {
-    const newProject = {
-      id: Math.random().toString(),
+    const newProject = new Project(
+      Math.random().toString(),
       title,
       description,
-      people: numPeople
-    };
+      numPeople,
+      ProjectStatus.Active
+    );
     this.projects.push(newProject);
     for (const listenerFunc of this.listeners) {
       listenerFunc(this.projects.slice());
@@ -86,7 +107,7 @@ class ProjectList {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLElement;
-  assignedProjects: any[];
+  assignedProjects: Project[];
 
   constructor(private type: 'active' | 'finished') {
     this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
@@ -97,7 +118,7 @@ class ProjectList {
     this.element = importNode.firstElementChild as HTMLElement;
     this.element.id = `${this.type}-projects`;
 
-    projectState.addListener((projects: any[]) => {
+    projectState.addListener((projects: Project[]) => {
       this.assignedProjects = projects;
       this.renderProjects();
     });

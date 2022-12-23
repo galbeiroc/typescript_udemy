f67cf482a5b0589526a536b1e9b4cbb955ae583f
galbeiroc <alcresgu_1989@hotmail.com>
2022-12-23 15:45:34 -0500

    set project to work with webpack

---
+++ webpack-ts/src/decorators/autobind.ts
@@ -0,0 +1,18 @@
+// autobind decorator
+
+export function autobind(
+  _: any,
+  _2: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjustedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFunction = originalMethod.bind(this);
+      return boundFunction;
+    }
+  };
+  return adjustedDescriptor;
+}
+

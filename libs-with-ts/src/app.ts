97b493f187458da82f576ed699226fef5857d916
galbeiroc <alcresgu_1989@hotmail.com>
2022-12-24 10:51:47 -0500

    work with class validator

--- libs-with-ts/src/app.ts
+++ libs-with-ts/src/app.ts
@@ -1,11 +1,21 @@
 import 'reflect-metadata';
 import { plainToClass } from 'class-transformer';
 import { Product } from "./product.model";
+import { validate } from 'class-validator';
 
 const products = [
   { title: 'A Carpet', price: 25.38 },
   { title: 'A Book', price: 18.99 }
 ]
+
+const newProd = new Product('', -8.58);
+validate(newProd).then(err => {
+  if (err.length > 0) {
+    console.log('VALIDATIONS ERROS');
+    console.log(err);
+  }
+})
+console.log(newProd.getInformation())
 
 // const loadedProducts = products.map(prod => {
 //   return new Product(prod.title, prod.price);

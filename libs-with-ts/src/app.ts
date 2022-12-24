8eb30e8578c0ad6a78df32080dd397f0923fb218
galbeiroc <alcresgu_1989@hotmail.com>
2022-12-23 23:14:03 -0500

    implement class-transformer

--- libs-with-ts/src/app.ts
+++ libs-with-ts/src/app.ts
@@ -1,6 +1,20 @@
-import _ from 'lodash';
-declare var GLOBAL: string;
+import 'reflect-metadata';
+import { plainToClass } from 'class-transformer';
+import { Product } from "./product.model";
 
-console.log(_.shuffle([11, 2, 3]));
+const products = [
+  { title: 'A Carpet', price: 25.38 },
+  { title: 'A Book', price: 18.99 }
+]
 
-console.log(GLOBAL);
+// const loadedProducts = products.map(prod => {
+//   return new Product(prod.title, prod.price);
+// })
+
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
+
+// const p1 = new Product('A Book', 18.99);

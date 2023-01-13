a078a8588bba09d87a24579c01a53a30068d5b86
galbeiroc <alcresgu_1989@hotmail.com>
2023-01-13 17:50:27 -0500

    render map with google maps

--- prj-select-and-share-place/src/app.ts
+++ prj-select-and-share-place/src/app.ts
@@ -14,20 +14,26 @@ type GoogleGecodingResponse = {
 
 function searchAddressHandler(e: Event) {
   e.preventDefault();
-  const enteredAddress = addressInput.value
-  console.log('clicked!!');
+  const enteredAddress = addressInput.value;
 
-  const GOOGLE_API_KEY = 'xxxxx';
-  // send this to Google API!
+  const GOOGLE_API_KEY = 'xxxxxxx';
+
   axios.get<GoogleGecodingResponse>(
     `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
       enteredAddress
     )}&key=${GOOGLE_API_KEY}`
   ).then(response => {
+    console.log(response.data.status);
     if (response.data.status !== 'OK') {
       throw new Error('Could not fetch location!');
     }
     const coordinates = response.data.results[0].geometry.location;
+    const map = new google.maps.Map(document.getElementById('map')!, {
+      center: coordinates,
+      zoom: 16
+    });
+    
+    new google.maps.Marker({ position: coordinates, map })
   }).catch(err => {
     alert(err.message);
     console.log(err);

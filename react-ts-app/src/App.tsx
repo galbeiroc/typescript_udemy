8a7dbe15fd3ab4effe9967d43d5d83f5c50ce570
galbeiroc <alcresgu_1989@hotmail.com>
2023-01-18 19:22:38 -0500

    work with props

--- react-ts-app/src/App.tsx
+++ react-ts-app/src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import TodoList from './components/TodoList';
 
 const App: React.FC = () => {
+  const todos = [
+    { id: 't1', text: 'Finish Project' }
+  ];
+
   return (
     <div className="App">
       <h1>React TS - App</h1>
+      <TodoList items={todos} />
     </div>
   );
 }

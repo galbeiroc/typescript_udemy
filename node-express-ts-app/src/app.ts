17d5808587d21015f63af9c8fc01d47f64bac0c1
galbeiroc <alcresgu_1989@hotmail.com>
2023-01-21 13:41:20 -0500

    add middleware types

--- node-express-ts-app/src/app.ts
+++ node-express-ts-app/src/app.ts
@@ -1,5 +1,13 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+
+import todosRouter from './routes/todos';
 
 const app = express();
+
+app.use('/todos', todosRouter);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
 
 app.listen(3000);
